d76c1ab01ac07dff5f4758a17e92801142ea3d17
Wally <wally@imwally.net>
2015-11-11 23:43:12 -0500

    Add printTLSInfo function

--- certify.go
+++ certify.go
@@ -10,6 +10,40 @@ import (
 	"regexp"
 )
 
+var TLSCipherSuite = map[uint16]string{
+	tls.TLS_RSA_WITH_RC4_128_SHA:                "TLS_RSA_WITH_RC4_128_SHA",
+	tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA:           "TLS_RSA_WITH_3DES_EDE_CBC_SHA",
+	tls.TLS_RSA_WITH_AES_128_CBC_SHA:            "TLS_RSA_WITH_AES_128_CBC_SHA",
+	tls.TLS_RSA_WITH_AES_256_CBC_SHA:            "TLS_RSA_WITH_AES_256_CBC_SHA",
+	tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:        "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:    "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:    "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+	tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA:          "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
+	tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:     "TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA",
+	tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:      "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:      "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:   "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256: "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:   "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384: "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+}
+
+var TLSVersion = map[uint16]string{
+	tls.VersionSSL30: "SSL v3.0",
+	tls.VersionTLS10: "TLS v1.0",
+	tls.VersionTLS11: "TLS v1.1",
+	tls.VersionTLS12: "TLS v1.2",
+}
+
+func printTLSInfo(state tls.ConnectionState) {
+
+	version := TLSVersion[state.Version]
+	suite := TLSCipherSuite[state.CipherSuite]
+
+	fmt.Println("Version: ", version)
+	fmt.Println("Cipher Suite: ", suite)
+}
+
 func findURL(s []string) (url string, err error) {
 	validURL := regexp.MustCompile(`^https://[a-z]`)
 
@@ -28,7 +62,7 @@ func Certify(s []string) {
 		log.Fatal(err)
 		return
 	}
-	
+
 	u, err := url.Parse(found)
 	if err != nil {
 		log.Fatal(err)
@@ -39,6 +73,8 @@ func Certify(s []string) {
 		log.Fatal(err.Error())
 	}
 	defer conn.Close()
+
+	printTLSInfo(conn.ConnectionState())
 }
 
 func main() {
